de9e38c37500874f603c4f02cdfcf6e180c1f778
Fredrik Forssen <fredde@grodslok.net>
2016-12-07 21:55:16 +0100

    Start supporting fable 0.7

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,5 @@
+### 0.2.0
+- Upgrade to fable 0.7 and convert to new package distribution type.
+
+### 0.1.2
+- Initial stable release on npm
--- src/Fable.Helpers.ReactRedux.fs
+++ src/Fable.Helpers.ReactRedux.fs
@@ -1,21 +1,22 @@
-﻿[<Fable.Core.Erase>]
-module Fable.Helpers.ReactRedux
+﻿module Fable.Helpers.ReactRedux
 
 open Fable.Core
 open Fable.Import
 open Fable.Core.JsInterop
 
-let inline private convertAndWrap<'a> (fn : ('a -> (string * obj) list)) =
+[<PassGenerics>]
+let private convertAndWrap<'a> (fn : ('a -> (string * obj) list)) =
     let f = System.Func<_, _> (fun x -> x |> fn |> createObj) 
     f |> box |> Some
 
-let inline private convertAndWrapWithProps<'a, 'b> (fn : ('a -> 'b -> (string * obj) list)) =
+[<PassGenerics>]
+let private convertAndWrapWithProps<'a, 'b> (fn : ('a -> 'b -> (string * obj) list)) =
     let f = System.Func<_, _, _> (fun x y -> 
         fn x y 
         |> createObj) 
     f |> box |> Some
 
-let inline private createReduxConnector (statemapper : obj option) (dispatchmapper : obj option) =
+let private createReduxConnector (statemapper : obj option) (dispatchmapper : obj option) =
     match statemapper, dispatchmapper with
     | Some(s), Some(d) -> 
         ReactReduxImport.connect(s, d)
@@ -26,7 +27,7 @@ let inline private createReduxConnector (statemapper : obj option) (dispatchmapp
     | None, None -> 
         failwith "No mapper provided!"
 
-type Inner<'TState, 'TProps> = 
+type private Inner<'TState, 'TProps> = 
     ('TState -> (string * obj) list) option *
     ('TState -> 'TProps -> (string * obj) list) option *
     (ReactRedux.Dispatcher -> (string * obj) list) option *
@@ -36,37 +37,33 @@ type Inner<'TState, 'TProps> =
 type ConnectorBuilder<'TState, 'TProps> =
     | Connector of Inner<'TState, 'TProps>
         
-let inline createConnector () =
+let createConnector () =
     ConnectorBuilder.Connector(None, None, None, None)
 
-let inline withStateMapper (sm : ('S -> (string * obj) list)) (c : ConnectorBuilder<'S, 'P>) =
+let withStateMapper (sm : ('S -> (string * obj) list)) (c : ConnectorBuilder<'S, 'P>) =
     match c with
     | Connector(_, _, dm, dmp) -> ConnectorBuilder.Connector(Some sm, None, dm, dmp)
 
-let inline withStateMapperWithProps (smp : ('S -> 'P -> (string * obj) list)) (c : ConnectorBuilder<'S, 'P>) =
+let withStateMapperWithProps (smp : ('S -> 'P -> (string * obj) list)) (c : ConnectorBuilder<'S, 'P>) =
     match c with
     | Connector(_, _, dm, dmp) -> ConnectorBuilder.Connector(None, Some smp, dm, dmp)
 
-let inline withDispatchMapper (dm : (ReactRedux.Dispatcher -> (string * obj) list)) (c : ConnectorBuilder<'S, 'P>) =
+let withDispatchMapper (dm : (ReactRedux.Dispatcher -> (string * obj) list)) (c : ConnectorBuilder<'S, 'P>) =
     match c with
     | Connector(sm, smp, _, _) -> ConnectorBuilder.Connector(sm, smp, Some dm, None)
 
-let inline withDispatchMapperWithProps (dmp : (ReactRedux.Dispatcher -> 'P -> (string * obj) list)) (c : ConnectorBuilder<'S, 'P>) =
+let withDispatchMapperWithProps (dmp : (ReactRedux.Dispatcher -> 'P -> (string * obj) list)) (c : ConnectorBuilder<'S, 'P>) =
     match c with
     | Connector(sm, smp, _, _) -> ConnectorBuilder.Connector(sm, smp, None, Some dmp)
 
-let inline private appendChildren
+let private appendChildren
         (props : 'TProps)
-        (children : Fable.Import.React.ReactElement<obj> list)
+        (children : Fable.Import.React.ReactElement list)
         (cmp : React.ComponentClass<'TProps>) =
-    let nodechildren = 
-        children
-        |> List.map (unbox<React.ReactNode>) 
-        |> List.toArray
-    React.createElement(cmp, props, nodechildren) 
-    |> unbox<React.ReactElement<obj>>
+    React.from cmp props children
 
-let inline getMappers<'TState, 'TProps> (c : ConnectorBuilder<'TState, 'TProps>) = 
+[<PassGenerics>]
+let private getMappers<'TState, 'TProps> (c : ConnectorBuilder<'TState, 'TProps>) = 
     let inner = c |> unbox<Inner<'TState, 'TProps>>
     let stateMapper =
         match inner with
@@ -80,9 +77,10 @@ let inline getMappers<'TState, 'TProps> (c : ConnectorBuilder<'TState, 'TProps>)
         | _, _, _, Some(dmp) -> convertAndWrapWithProps dmp
     stateMapper, dispatchMapper
 
-let inline buildComponent<'TComponent, 'TProps, 'TCtx, 'TState when 'TComponent :> React.Component<'TProps, 'TCtx>> 
+[<PassGenerics>]
+let buildComponent<'TComponent, 'TProps, 'TCtx, 'TState when 'TComponent :> React.Component<'TProps, 'TCtx>> 
     (props : 'TProps) 
-    (children : Fable.Import.React.ReactElement<obj> list)
+    (children : Fable.Import.React.ReactElement list)
     (c : ConnectorBuilder<'TState, 'TProps>) = 
         let stateMapper, dispatchMapper = getMappers<'TState, 'TProps> c
         createReduxConnector stateMapper dispatchMapper
@@ -90,10 +88,11 @@ let inline buildComponent<'TComponent, 'TProps, 'TCtx, 'TState when 'TComponent 
         |> unbox<React.ComponentClass<'TProps>>
         |> appendChildren props children
 
-let inline buildFunction<'TProps, 'TState> 
-    (fn : ('TProps -> React.ReactElement<obj>))
+[<PassGenerics>]
+let buildFunction<'TProps, 'TState> 
+    (fn : ('TProps -> React.ReactElement))
     (props : 'TProps) 
-    (children : Fable.Import.React.ReactElement<obj> list)
+    (children : Fable.Import.React.ReactElement list)
     (c : ConnectorBuilder<'TState, 'TProps>) = 
         let stateMapper, dispatchMapper = getMappers<'TState, 'TProps> c
         createReduxConnector stateMapper dispatchMapper
@@ -101,6 +100,6 @@ let inline buildFunction<'TProps, 'TState>
         |> unbox<React.ComponentClass<'TProps>>
         |> appendChildren props children
 
-
-let inline createProvider<'TStore when 'TStore :> Redux.IUntypedStore> (store : 'TStore)  (app : React.ReactElement<obj>) =
+[<PassGenerics>]
+let createProvider<'TStore when 'TStore :> Redux.IUntypedStore> (store : 'TStore)  (app : React.ReactElement) =
     Fable.Helpers.React.com<ReactRedux.Provider<_, _>, _, _> (createObj [ "store" ==> store ]) [ app ]
