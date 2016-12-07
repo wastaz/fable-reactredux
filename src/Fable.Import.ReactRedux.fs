d8f33c7513e0a13ad04be609861f912bc891b7ff
Fredrik Forssen <fredde@grodslok.net>
2016-12-07 21:55:16 +0100

    Start supporting fable 0.7

---
+++ src/Fable.Import.ReactRedux.fs
@@ -0,0 +1,18 @@
+﻿namespace Fable.Import
+
+open Fable.Core
+
+module ReactRedux =
+    type [<Import("Provider", "react-redux")>] Provider<'P, 'C>(props : 'P, context) =
+        inherit Fable.Import.React.Component<'P, 'C>(props)
+
+    type IDispatchable = interface end
+
+    type Dispatcher = IDispatchable -> unit
+
+    type Globals =
+        abstract connect : state : 'a * ?dispatch : 'b -> (React.Component<'TProps, 'TContext> -> React.Component<'TProps, 'TComponent>)
+
+[<AutoOpen>]
+module ReactRedux_Extensions =
+    let [<Import("*", "react-redux")>] ReactReduxImport : ReactRedux.Globals = jsNative
